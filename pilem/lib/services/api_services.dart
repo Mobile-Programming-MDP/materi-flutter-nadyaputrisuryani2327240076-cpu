8b1e81942ad9aa17f4b66d79973a704f0bffbc4c
nadyaputrisuryani <nputrisuryani@gmail.com>
2026-03-05 12:15:13 +0700

    Update api_services.dart

--- pilem/lib/services/api_services.dart
+++ pilem/lib/services/api_services.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:http/http.dart' as http;
+import 'package:http/http.dart'as http; 
 
 class ApiService {
   static const String baseUrl = "https://api.themoviedb.org/3";
